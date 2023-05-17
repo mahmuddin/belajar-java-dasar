42c88f97dc189cfee72f7cc1ca13c0e916a32589
Mahmuddin Nurul Fajri <mahmuddinnf@gmail.com>
2023-05-18 06:25:11 +0700

    added Break and DoWhileLoop

---
+++ java-dasar/src/DoWhileLoop.java
@@ -0,0 +1,11 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+//        var counter = 1;
+        var counter = 100;
+
+        do {
+            System.out.println("Perulangan Ke - " + counter);
+            counter++;
+        } while (counter <= 10);
+    }
+}
