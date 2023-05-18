3a086310a72e605ec9720100ddd0e517eda4ba54
Mahmuddin Nurul Fajri <mahmuddinnf@gmail.com>
2023-05-18 09:38:51 +0700

    Added Komentar

---
+++ java-dasar/src/Komentar.java
@@ -0,0 +1,19 @@
+public class Komentar {
+    public static void main(String[] args) {
+        System.out.println(sum(10, 20));
+    }
+
+    /**
+     * Menghitung jumlah a + b
+     *
+     * @param a nilai a
+     * @param b nilai b
+     * @return a + b
+     * @
+     */
+    static int sum(int a, int b) {
+        // Jumlahkan a + b
+        return a + b;
+    }
+
+}
